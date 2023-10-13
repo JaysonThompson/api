c5735508e67d1c9faff4357999b406c7d9463da5
Jayson Thompson <jaysonthompson922@gmail.com>
2023-10-13 07:52:30 -0700

    Initial commit

---
+++ routes/users.js
@@ -0,0 +1,23 @@
+import express from "express";
+
+import {
+	createUser,
+	getUsers,
+	deleteUser,
+	getUser,
+	patchUser,
+} from "../controllers/users.js";
+
+const router = express.Router();
+
+router.get("/", getUsers);
+
+router.post("/", createUser);
+
+router.get("/:id", getUser);
+
+router.delete("/:id", deleteUser);
+
+router.patch("/:id", patchUser);
+
+export default router;
